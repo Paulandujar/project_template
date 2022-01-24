eb655e7efa69b5e902d43f4486166bb2432ecf32
Irene Romero Granados <61683312+ireneeromero@users.noreply.github.com>
2022-01-24 19:26:49 +0100

    Mapeo

--- code/covid_network.R
+++ code/covid_network.R
@@ -7,3 +7,12 @@ data <- read.csv("results/uniprot1.csv", header=T, sep=";")
 data2 <- dplyr::filter(data, grepl("HUMAN",Entry.Name))
 data2$Entry.Name2 <- sapply(data2$Entry.Name, function(i) gsub("_HUMAN", "", i))
 
+# Mapeamos con STRING
+string_db <- STRINGdb$new( version="11", species=9606, score_threshold=400, input_directory="" )
+data_mapped <- string_db$map(data2, "Entry", removeUnmappedRows = TRUE )
+
+# Guardamos la red de proteinas
+hits <-data_mapped$STRING_id
+png("01_string_hits.png")
+string_db$plot_network(hits)
+dev.off()
