ae18918efe71c970cef67445ac2da71ef13eb3b2
paulandujar <paulandujar@uma.es>
2022-01-24 18:23:40 +0100

    Carga y filtrado de datos

---
+++ code/covid_network.R
@@ -0,0 +1,9 @@
+library(dplyr)
+
+# Cargamos el archivo de datos
+data <- read.csv("results/uniprot1.csv", header=T, sep=";")
+
+# Filtramos las entradas y cambiamos el Entry name 
+data2 <- dplyr::filter(data, grepl("HUMAN",Entry.Name))
+data2$Entry.Name2 <- sapply(data2$Entry.Name, function(i) gsub("_HUMAN", "", i))
+
