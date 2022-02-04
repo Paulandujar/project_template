5e659c8224f8ede31564a5d63dccb2a95e4e34e7
delcastillosoledad <delcastillosoledad@uma.es>
2022-02-04 11:06:44 +0100

    Prueba setup.sh

--- code/setup.sh
+++ code/setup.sh
@@ -7,27 +7,28 @@ chmod 755 software/deps_r
 
 #Instalacion de paquetes necesarios
 
-# brew install libffi ESTO ES PARA MAC 
+# brew install libffi
+# sudo apt-get install curl libssl-dev libcurl4-openssl-dev libxml2-dev
 
 #Instalacion de librerias R
-
-PKGS_CRAN="'BiocManager' 'zoo' 'igraph' 'linkcomm' 'data.table' 'devtools' 'dplyr'"
+#"'BiocManager''zoo' 'igraph' 'linkcomm' 'data.table' 'devtools' 'dplyr'"
+PKGS_CRAN="'BiocManager' 'RCurl' 'zoo' 'igraph' 'linkcomm' 'data.table' 'devtools' 'dplyr'"
 PKGS_BM="'STRINGdb'"
 
 for PKG_CRAN in $PKGS_CRAN
     do
-        Rscript -e 'install.packages('$PKG_CRAN', repos="https://cran.rstudio.com/", lib="software/deps_r" )'
+        Rscript -e 'install.packages('$PKG_CRAN', repos="https://cran.rstudio.com/", lib="software/deps_r", force = TRUE )'
         Rscript -e 'library('$PKG_CRAN', lib.loc="software/deps_r")'
     done
-    
-Rscript -e 'if (!requireNamespace("BiocManager", quietly = TRUE))
-install.packages("BiocManager")
-BiocManager::install(version = "3.14",ask=FALSE)'
+
+#Rscript -e 'if (!requireNamespace("BiocManager", quietly = TRUE))
+#install.packages("BiocManager")
+#BiocManager::install(version = "3.14",ask=FALSE)'
 
 for PKG_BM in $PKGS_BM
     do
-        Rscript -e 'BiocManager::install('$PKG_BM', lib="software/deps_r" )'
-        Rscript -e 'library('$PKG_BM',lib.loc="software/deps_r")'
+        Rscript -e 'library("BiocManager", lib.loc = "software/deps_r") ; install('$PKG_BM', lib="software/deps_r", force = TRUE )'
+        Rscript -e 'library('$PKG_BM', lib.loc="software/deps_r")'
     done
     
 #Añadir el path a la variable libPaths
