9a23050a707e1b36d74ece8a1d8eb9faa84b621e
delcastillosoledad <delcastillosoledad@uma.es>
2022-02-04 11:35:01 +0100

    Setup

--- code/setup.sh
+++ code/setup.sh
@@ -1,38 +1,37 @@
 #Creacion del directorio para las librerias
 
-chmod 777 setup.sh
 mkdir software software/deps_r
 chmod 755 software/deps_r
 
 
 #Instalacion de paquetes necesarios
 
-# brew install libffi
+brew install libffi openssl
 # sudo apt-get install curl libssl-dev libcurl4-openssl-dev libxml2-dev
 
 #Instalacion de librerias R
-#"'BiocManager''zoo' 'igraph' 'linkcomm' 'data.table' 'devtools' 'dplyr'"
-PKGS_CRAN="'BiocManager' 'RCurl' 'zoo' 'igraph' 'linkcomm' 'data.table' 'devtools' 'dplyr'"
+#"'BiocManager''zoo' 'igraph' 'linkcomm' 'data.table' 'devtools' 'dplyr' 'RCurl'"
+PKGS_CRAN="'BiocManager' 'zoo' 'igraph' 'linkcomm' 'data.table' 'devtools' 'dplyr' 'RCurl'"
 PKGS_BM="'STRINGdb'"
+R_INSTALL_DIR="'software/deps_r'"
 
 for PKG_CRAN in $PKGS_CRAN
     do
-        Rscript -e 'install.packages('$PKG_CRAN', repos="https://cran.rstudio.com/", lib="software/deps_r", force = TRUE )'
-        Rscript -e 'library('$PKG_CRAN', lib.loc="software/deps_r")'
+        Rscript -e 'install.packages('$PKG_CRAN', repos="https://cran.rstudio.com/", lib='$R_INSTALL_DIR', force = TRUE )'
+        Rscript -e 'library('$PKG_CRAN', lib.loc='$R_INSTALL_DIR')'
     done
 
-#Rscript -e 'if (!requireNamespace("BiocManager", quietly = TRUE))
-#install.packages("BiocManager")
-#BiocManager::install(version = "3.14",ask=FALSE)'
+Rscript -e 'if (!requireNamespace("BiocManager", quietly = TRUE))
+install.packages("BiocManager")
+BiocManager::install(version = "3.14",ask=FALSE)'
 
 for PKG_BM in $PKGS_BM
     do
-        Rscript -e 'library("BiocManager", lib.loc = "software/deps_r") ; install('$PKG_BM', lib="software/deps_r", force = TRUE )'
-        Rscript -e 'library('$PKG_BM', lib.loc="software/deps_r")'
+        Rscript -e 'library("BiocManager", lib.loc = '$R_INSTALL_DIR') ; install('$PKG_BM', lib='$R_INSTALL_DIR', force = TRUE )'
+        Rscript -e 'library('$PKG_BM', lib.loc='$R_INSTALL_DIR')'
     done
     
 #Añadir el path a la variable libPaths
 
-Rscript -e '.libPaths(c(.libPaths(),paste(getwd(),"software/deps_r",sep="/"))))'
-echo "*********************** INSTALLATION SUCCESSFULLY *****************************"
-echo "Librerias cargadas en el directorio /software/deps_r"
+Rscript -e '.libPaths(c(.libPaths(),paste(getwd(),'$R_INSTALL_DIR',sep="/")))'
+echo "Librerias cargadas en el directorio $R_INSTALL_DIR"
